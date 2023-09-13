bab61317400b36145b508519aab65fb505c39537
Mighty-1 <133228891+Mighty-1@users.noreply.github.com>
2023-09-13 03:45:36 +0100

    Updated builtins_list.c

--- builtins_list.c
+++ builtins_list.c
@@ -10,19 +10,19 @@ int builtins_list(vars_of_project *data)
 {
 	int iter;
 	builtins options[] = {
-		{"exit", builtin_exit},
-		{"help", builtin_help},
-		{"cd", builtin_cd},
-		{"alias", builtin_alias},
-		{"env", builtin_env},
-		{"setenv", builtin_set_env},
-		{"unsetenv", builtin_unset_env},
+		{"exit", exit_built},
+		{"help", help_built},
+		{"cd", cd_built},
+		{"alias", alias_built},
+		{"env", environ_built},
+		{"setenv", built_set_environ},
+		{"unsetenv", built_unset_environ},
 		{NULL, NULL}
 	};
 
 	for (iter = 0; options[iter].builtin != NULL; iter++)
 	{
-		if (str_compare(options[iter].builtin, data->cmd_name, 0))
+		if (string_cmp(options[iter].builtin, data->cmd_name, 0))
 		{
 			return (options[iter].function(data));
 		}
