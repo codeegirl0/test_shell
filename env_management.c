76b4fbe55c71c0a2241c281f0c7f8fe091453652
Mighty-1 <133228891+Mighty-1@users.noreply.github.com>
2023-09-13 14:39:59 +0100

    Updated env_management.c

--- env_management.c
+++ env_management.c
@@ -1,23 +1,23 @@
 #include "shell.h"
 
 /**
- * env_get_key - to get env var value
+ * environ_key_get - to get env var value
  * @key: interest env variable
  * @data: program data struct
  * Return:  value as pointer or NULL
  */
-char *env_get_key(char *key, vars_of_project *data)
+char *environ_key_get(char *key, vars_of_project *data)
 {
 	int m, len_key = 0;
 
 	if (key == NULL || data->envir == NULL)
 		return (NULL);
 
-	len_key = str_length(key);
+	len_key = string_len(key);
 
 	for (m = 0; data->envir[m]; m++)
 	{
-		if (str_compare(key, data->envir[m], len_key) &&
+		if (string_cmp(key, data->envir[m], len_key) &&
 		 data->envir[m][len_key] == '=')
 		{
 			return (data->envir[m] + len_key + 1);
@@ -27,25 +27,25 @@ char *env_get_key(char *key, vars_of_project *data)
 }
 
 /**
- * env_set_key - to set a key
+ * environ_key_set - to set a key
  * @key: variable to set
  * @value: the new data
  * @data: program data struct
  * Return: 1 if null or 2 if error 0 if sucess.
  */
 
-int env_set_key(char *key, char *value, vars_of_project *data)
+int environ_key_set(char *key, char *value, vars_of_project *data)
 {
 	int m, len_key = 0, the_new_key = 1;
 
 	if (key == NULL || value == NULL || data->envir == NULL)
 		return (1);
 
-	len_key = str_length(key);
+	len_key = string_len(key);
 
 	for (m = 0; data->envir[m]; m++)
 	{
-		if (str_compare(key, data->envir[m], len_key) &&
+		if (string_cmp(key, data->envir[m], len_key) &&
 		 data->envir[m][len_key] == '=')
 		{
 			the_new_key = 0;
@@ -53,8 +53,8 @@ int env_set_key(char *key, char *value, vars_of_project *data)
 			break;
 		}
 	}
-	data->envir[m] = str_concat(str_duplicate(key), "=");
-	data->envir[m] = str_concat(data->envir[m], value);
+	data->envir[m] = str_concati_nate(string_dup(key), "=");
+	data->envir[m] = str_concati_nate(data->envir[m], value);
 
 	if (the_new_key)
 	{
@@ -64,23 +64,23 @@ int env_set_key(char *key, char *value, vars_of_project *data)
 }
 
 /**
- * env_remove_key - to remove key from env
+ * environ_rmv_key - to remove key from env
  * @key: remove the key
  * @data: program data struct
  * Return: 1 if the key removed or 0 if the key is not exist;
  */
-int env_remove_key(char *key, vars_of_project *data)
+int environ_rmv_key(char *key, vars_of_project *data)
 {
 	int m, len_key = 0;
 
 	if (key == NULL || data->envir == NULL)
 		return (0);
 
-	len_key = str_length(key);
+	len_key = string_len(key);
 
 	for (m = 0; data->envir[m]; m++)
 	{
-		if (str_compare(key, data->envir[m], len_key) &&
+		if (string_cmp(key, data->envir[m], len_key) &&
 		 data->envir[m][len_key] == '=')
 		{
 			free(data->envir[m]);
@@ -99,17 +99,17 @@ int env_remove_key(char *key, vars_of_project *data)
 
 
 /**
- * print_environ - to print current env
+ * prt_env - to print current env
  * @data: program data struct
  * Return: nothing
  */
-void print_environ(vars_of_project *data)
+void prt_env(vars_of_project *data)
 {
 	int n;
 
 	for (n = 0; data->envir[n]; n++)
 	{
-		_print(data->envir[n]);
-		_print("\n");
+		_prt(data->envir[n]);
+		_prt("\n");
 	}
 }
