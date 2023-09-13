fcda9209c190292dd12da2f550403c454b05e0bd
Mighty-1 <133228891+Mighty-1@users.noreply.github.com>
2023-09-13 14:55:34 +0100

    Updated helpers_string.c

--- helpers_string.c
+++ helpers_string.c
@@ -1,11 +1,11 @@
 #include "shell.h"
 
 /**
- * str_length - returns the length of a string.
+ * string_len - returns the length of a string.
  * @string: pointer to string.
  * Return: length of string.
  */
-int str_length(char *string)
+int string_len(char *string)
 {
 	int length = 0;
 
@@ -19,11 +19,11 @@ int str_length(char *string)
 }
 
 /**
- * str_duplicate - duplicates an string
+ * string_dup - duplicates an string
  * @string: String to be copied
  * Return: pointer to the array
  */
-char *str_duplicate(char *string)
+char *string_dup(char *string)
 {
 	char *result;
 	int length, i;
@@ -31,7 +31,7 @@ char *str_duplicate(char *string)
 	if (string == NULL)
 		return (NULL);
 
-	length = str_length(string) + 1;
+	length = string_len(string) + 1;
 
 	result = malloc(sizeof(char) * length);
 
@@ -50,13 +50,13 @@ char *str_duplicate(char *string)
 }
 
 /**
- * str_compare - Compare two strings
+ * string_cmp - Compare two strings
  * @string1: String one, or the shorter
  * @string2: String two, or the longer
  * @number: number of characters to be compared, 0 if infinite
  * Return: 1 if the strings are equals,0 if the strings are different
  */
-int str_compare(char *string1, char *string2, int number)
+int string_cmp(char *string1, char *string2, int number)
 {
 	int iterator;
 
@@ -68,7 +68,7 @@ int str_compare(char *string1, char *string2, int number)
 
 	if (number == 0) /* infinite longitud */
 	{
-		if (str_length(string1) != str_length(string2))
+		if (string_len(string1) != string_len(string2))
 			return (0);
 		for (iterator = 0; string1[iterator]; iterator++)
 		{
@@ -89,24 +89,24 @@ int str_compare(char *string1, char *string2, int number)
 }
 
 /**
- * str_concat - concatenates two strings.
+ * str_concati_nate - concatenates two strings.
  * @string1: String to be concatenated
  * @string2: String to be concatenated
  *
  * Return: pointer to the array
  */
-char *str_concat(char *string1, char *string2)
+char *str_concati_nate(char *string1, char *string2)
 {
 	char *result;
 	int length1 = 0, length2 = 0;
 
 	if (string1 == NULL)
 		string1 = "";
-	length1 = str_length(string1);
+	length1 = string_len(string1);
 
 	if (string2 == NULL)
 		string2 = "";
-	length2 = str_length(string2);
+	length2 = string_len(string2);
 
 	result = malloc(sizeof(char) * (length1 + length2 + 1));
 	if (result == NULL)
@@ -134,15 +134,15 @@ char *str_concat(char *string1, char *string2)
 
 
 /**
- * str_reverse - reverses a string.
+ * string_rev - reverses a string.
  *
  * @string: pointer to string.
  * Return: void.
  */
-void str_reverse(char *string)
+void string_rev(char *string)
 {
 
-	int i = 0, length = str_length(string) - 1;
+	int i = 0, length = string_len(string) - 1;
 	char hold;
 
 	while (i < length)
