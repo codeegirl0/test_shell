63b7146d1887e5903f9b325e82280a52040789ef
Mighty-1 <133228891+Mighty-1@users.noreply.github.com>
2023-09-13 14:45:04 +0100

    Updated helpers_free.c

--- helpers_free.c
+++ helpers_free.c
@@ -1,14 +1,14 @@
 #include "shell.h"
 
 /**
- * free_recurrent_data - free the fields needed each loop
+ * freeing_recurrentData - free the fields needed each loop
  * @data: struct of the program's data
  * Return: Nothing
  */
-void free_recurrent_data(data_of_program *data)
+void freeing_recurrentData(data_of_program *data)
 {
 	if (data->tokens)
-		free_array_of_pointers(data->tokens);
+		free_arr_of_ptrs(data->tokens);
 	if (data->input_line)
 		free(data->input_line);
 	if (data->command_name)
@@ -20,29 +20,29 @@ void free_recurrent_data(data_of_program *data)
 }
 
 /**
- * free_all_data - free all field of the data
+ * freeing_allData - free all field of the data
  * @data: struct of the program's data
  * Return: Nothing
  */
-void free_all_data(data_of_program *data)
+void freeing_allData(data_of_program *data)
 {
 	if (data->file_descriptor != 0)
 	{
 		if (close(data->file_descriptor))
 			perror(data->program_name);
 	}
-	free_recurrent_data(data);
-	free_array_of_pointers(data->env);
-	free_array_of_pointers(data->alias_list);
+	freeing_recurrentData(data);
+	free_arr_of_ptrs(data->env);
+	free_arr_of_ptrs(data->alias_list);
 }
 
 /**
- * free_array_of_pointers - frees each pointer of an array of pointers and the
+ * free_arr_of_ptrs - frees each pointer of an array of pointers and the
  * array too
  * @array: array of pointers
  * Return: nothing
  */
-void free_array_of_pointers(char **array)
+void free_arr_of_ptrs(char **array)
 {
 	int i;
 
